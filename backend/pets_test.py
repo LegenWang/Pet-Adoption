6a0ae137185d232a53d092026b8d01ce1cf635a8
Alexander Torino <alexander.torino@uconn.edu>
2024-10-31 19:09:24 -0400

    changed pets_test

--- backend/pets_test.py
+++ backend/pets_test.py
@@ -3,31 +3,36 @@ import pytest
 from flask import Flask
 from pets import pets_blueprint
 
-@pytest.fixture
-def client():
-    """ Configure the test client."""
-    app = Flask(__name__)
-    app.register_blueprint(pets_blueprint)
-    app.config['TESTING'] = True
-    with app.test_client() as test_client:
-        yield test_client
+class TestAPI:
+    ''' class for all the unittests'''
 
-def test_get_pets(client):
-    """Test the GET / endpoint"""
-    response = client.get('/pets')
-    assert response.status_code == 200
-    data = response.get_json()
-    assert isinstance(data, list)  # Check that response is a list
+    client = None
 
-def test_get_pet_found(client):
-    """Test retrieving a pet by ID that exists"""
-    response = client.get('/pets/1')
-    assert response.status_code == 200
-    data = response.get_json()
-    assert 'Buddy' in data
+    @pytest.fixture(autouse=True, scope='function')
+    def setup_client(self):
+        """Setting up test client for all tests"""
+        app = Flask(__name__)
+        app.register_blueprint(pets_blueprint)
+        app.config['TESTING'] = True
+        with app.test_client() as test_client:
+            TestAPI.client = test_client
+            yield
 
-def test_get_pet_not_found(client):
-    """Test retrieving a pet by ID that exists"""
-    response = client.get('/pets/999')
-    assert response.status_code == 404
+    def test_get_pets(client):
+        """Test the GET / endpoint"""
+        response = client.get('/pets')
+        assert response.status_code == 200
+        data = response.get_json()
+        assert isinstance(data, list)  # Check that response is a list
 
+    def test_get_pet_found(client):
+        """Test retrieving a pet by ID that exists"""
+        response = client.get('/pets/1')
+        assert response.status_code == 200
+        data = response.get_json()
+        assert 'Buddy' in data
+
+    def test_get_pet_not_found(client):
+        """Test retrieving a pet by ID that exists"""
+        response = client.get('/pets/999')
+        assert response.status_code == 404
