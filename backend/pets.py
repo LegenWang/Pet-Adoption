8f9e8dacac4ae5fed477fdc7dbf49a2f2f4d070a
Alexander Torino <alexander.torino@uconn.edu>
2024-10-30 22:15:14 -0400

    linting

--- backend/pets.py
+++ backend/pets.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, Blueprint
-pet_blueprint = Blueprint('pets', __name__)
+pets_blueprint = Blueprint('pets', __name__)
 
 pets = [
     {"id": 1, "name": "Buddy", "breed": "Golden Retriever", "age": "4 years", "adopted": False},
@@ -7,12 +7,12 @@ pets = [
     {"id": 3, "name": "Tucker", "breed": "Mixed", "age": "8 months", "adopted": False},
 ]
 
-@pet_blueprint.route('', methods=['GET'])
+@pets_blueprint.route('', methods=['GET'])
 def get_pets():
     """Returns all pet data"""
     return jsonify(pets), 200
 
-@pet_blueprint.route('/<int:pet_id>', methods=['GET'])
+@pets_blueprint.route('/<int:pet_id>', methods=['GET'])
 def get_pet(pet_id):
     """Returns the data of one pet based on the pet_id number"""
     for pet in pets:
