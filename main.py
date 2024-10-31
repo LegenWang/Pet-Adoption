7c1a69105388bd2d789155fbd9d30b65f03446d5
Gabriel Zambrano <gabriel.zambrano@uconn.edu>
2024-10-30 20:35:51 -0400

    edited main

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from flask import Flask '''main.py that routes the methods in the files'''
-
+from flask import Flask
 # Import your API blueprints
-from test import test_blueprint
 from pets import pets_blueprint
-
-
+from users import user_blueprint
+#from application import application_blueprint
 app = Flask(__name__)
 
 
 # Register blueprints
-app.register_blueprint(test_blueprint, url_prefix='/test')
 app.register_blueprint(pets_blueprint, url_prefix='/pets')
+app.register_blueprint(user_blueprint, url_prefix='/user')
+#app.register_blueprint(application_blueprint, url_prefix='/application')
+
 
 
 @app.route('/')
